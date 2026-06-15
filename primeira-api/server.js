c355c9528640e578f68e4496205bea044cf88aee
Fabricio <piresfabricio024@gmail.com>
2026-06-15 20:13:58 -0300

    primeira API: rotas GET de produtos

---
+++ primeira-api/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const produtos = [
+  { id: 1, nome: "Arroz", preco: 25 },
+  { id: 2, nome: "Feijão", preco: 18 },
+  { id: 3, nome: "Carne", preco: 45 },
+]
+
+app.get('/ola', (req, res) => {
+    res.json({mensagem: 'Olá Mundo'})
+})
+
+app.get('/produtos', (req, res) => {
+    res.json(produtos)
+})
+
+app.get('/produtos/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const produto = produtos.find(p => p.id === id)
+    if (!produto) {
+        res.status(404).json("Insira um ID que exista.")
+        return
+    }
+    res.json(produto)
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
